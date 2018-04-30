9200fe03d4a00d0ae44fbec2caf485f5b682afec
Tusotr <trantuansong22@gmail.com>
2018-04-30 21:37:16 +0700

    Updating with sideBar and js file

---
+++ blog.js
@@ -0,0 +1,10 @@
+
+ function openBar(){
+     var x = document.getElementsByClassName("side-bar")[0];
+     x.style.display = "block";
+ }
+
+ function closeBar(){
+     var x = document.getElementsByClassName("side-bar")[0];
+     x.style.display = "none";
+ }
